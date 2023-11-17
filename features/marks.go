5b7a05eaaefdeaa7fe94c2f2c9396f02d4eef65d
ManavMuthanna <muthannamanav2003@gmail.com>
2023-11-17 19:27:04 +0530

    feat: display SemIDs and SemNames in a table format

--- features/marks.go
+++ features/marks.go
@@ -7,21 +7,37 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+
+	"github.com/olekukonko/tablewriter"
+	"golang.org/x/net/html"
 )
 
-func SendReq() {
+type SemesterDetails struct {
+	SemNames []string
+	SemIds   []string
+}
+
+func fetchReq(target_url string) ([]byte, error) {
 	client := &http.Client{}
-	var data = strings.NewReader("------WebKitFormBoundary9yjNZXu7BBjgQK7J\r\nContent-Disposition: form-data; name=\"authorizedID\"\r\n\r\n21BIT0151\r\n------WebKitFormBoundary9yjNZXu7BBjgQK7J\r\nContent-Disposition: form-data; name=\"semesterSubId\"\r\n\r\nVL20232401\r\n------WebKitFormBoundary9yjNZXu7BBjgQK7J\r\nContent-Disposition: form-data; name=\"_csrf\"\r\n\r\n502d64c5-adb3-4d05-80fa-48fcdab9072e\r\n------WebKitFormBoundary9yjNZXu7BBjgQK7J--\r\n")
-	req, err := http.NewRequest("POST", "https://vtop.vit.ac.in/vtop/examinations/doStudentMarkView", data)
+
+	//variables that need to be set with every request
+	authID := "21BIT0151"
+	csrf := "82bca422-9254-4979-932a-738930917b14"
+	semID := ""
+	jsessionID := "01EBB69913CFF3E87F2F1729C5B17F69"
+
+	var data = strings.NewReader(fmt.Sprintf("------WebKitFormBoundary9yjNZXu7BBjgQK7J\r\nContent-Disposition: form-data; name=\"authorizedID\"\r\n\r\n%s\r\n------WebKitFormBoundary9yjNZXu7BBjgQK7J\r\nContent-Disposition: form-data; name=\"semesterSubId\"\r\n\r\n%s\r\n------WebKitFormBoundary9yjNZXu7BBjgQK7J\r\nContent-Disposition: form-data; name=\"_csrf\"\r\n\r\n%s\r\n------WebKitFormBoundary9yjNZXu7BBjgQK7J--\r\n", authID, semID, csrf))
+	req, err := http.NewRequest("POST", target_url, data)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	req.Header.Set("authority", "vtop.vit.ac.in")
 	req.Header.Set("accept", "*/*")
 	req.Header.Set("accept-language", "en-US,en;q=0.6")
 	req.Header.Set("content-type", "multipart/form-data; boundary=----WebKitFormBoundary9yjNZXu7BBjgQK7J")
-	req.Header.Set("cookie", "JSESSIONID=9E10429B72C1A8851454F17313472E99; SERVERID=s2")
+	req.Header.Set("cookie", fmt.Sprintf("JSESSIONID=%s; SERVERID=s2", jsessionID))
 	req.Header.Set("origin", "https://vtop.vit.ac.in")
 	req.Header.Set("referer", "https://vtop.vit.ac.in/vtop/content?")
 	req.Header.Set("sec-ch-ua", `"Brave";v="119", "Chromium";v="119", "Not?A_Brand";v="24"`)
@@ -35,12 +51,143 @@ func SendReq() {
 	req.Header.Set("x-requested-with", "XMLHttpRequest")
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 	bodyText, err := io.ReadAll(resp.Body)
 	if err != nil {
+		return nil, err
+	}
+	return bodyText, nil
+}
+
+func GetSemDetails() SemesterDetails {
+	url := "https://vtop.vit.ac.in/vtop/examinations/StudentMarkView"
+	bodyText, err := fetchReq(url)
+	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", bodyText)
+
+	// Parse the HTML
+	doc, err := html.Parse(strings.NewReader(string(bodyText)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a slice to store the extracted data
+	var SemNames []string
+	var SemIds []string
+
+	var tempIds []string
+	// Find and save the semester IDs
+	findAndSaveSemIds(doc, "form-control", &tempIds)
+
+	SemIds = removeEmptyStrings(tempIds)
+
+	// fmt.Printf("%q\n", SemIds)
+
+	for _, semId := range SemIds {
+		optionText := findOptionWithTagValue(doc, semId)
+
+		if optionText != "" {
+			SemNames = append(SemNames, optionText)
+		} else {
+			// Handle the case where no <option> tag is found with the specified SemId
+			SemNames = append(SemNames, "Unknown")
+		}
+	}
+
+	// Return the encapsulated struct
+	return SemesterDetails{
+		SemNames: SemNames,
+		SemIds:   SemIds,
+	}
+
+}
+
+func PrintSemDetails() {
+	semDetails := GetSemDetails()
+
+	// Create a table
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Index", "SemId", "SemName"})
+
+	// Iterate through SemIds and SemNames using a for loop
+	for i := 0; i < len(semDetails.SemIds); i++ {
+		index := fmt.Sprintf("%d", i+1)
+		semId := semDetails.SemIds[i]
+		semName := semDetails.SemNames[i]
+
+		// Add a row to the table
+		table.Append([]string{index, semId, semName})
+	}
+
+	// Render the table
+	table.Render()
+}
+
+func findAndSaveSemIds(n *html.Node, targetClass string, result *[]string) {
+	if n.Type == html.ElementNode && n.Data == "select" {
+		for _, attr := range n.Attr {
+			if attr.Key == "class" && strings.Contains(attr.Val, targetClass) {
+				// Process the <select> element
+				for c := n.FirstChild; c != nil; c = c.NextSibling {
+					if c.Type == html.ElementNode && c.Data == "option" {
+						for _, optAttr := range c.Attr {
+							if optAttr.Key == "value" {
+								// Save the value attribute to the result slice
+								*result = append(*result, optAttr.Val)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		findAndSaveSemIds(c, targetClass, result)
+	}
+}
+
+func removeEmptyStrings(data []string) []string {
+	var cleanedData []string
+	for _, item := range data {
+		if item != "" {
+			cleanedData = append(cleanedData, item)
+		}
+	}
+	return cleanedData
+}
+
+func findOptionWithTagValue(n *html.Node, targetValue string) string {
+	if n.Type == html.ElementNode && n.Data == "option" {
+		for _, attr := range n.Attr {
+			if attr.Key == "value" && attr.Val == targetValue {
+				// Found the <option> tag with the specified value, return its text content
+				return getTextContent(n)
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if result := findOptionWithTagValue(c, targetValue); result != "" {
+			return result
+		}
+	}
+
+	return ""
+}
+
+func getTextContent(n *html.Node) string {
+	var textContent string
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.TextNode {
+			textContent += c.Data
+		} else if c.Type == html.ElementNode {
+			textContent += getTextContent(c)
+		}
+	}
+
+	return textContent
 }
