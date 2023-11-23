0076974cb4f29bae950a9da646428b2dbbe9eb2d
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-11-24 02:27:41 +0530

    Feat: obtained login page
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- login/prelogin.go
+++ login/prelogin.go
@@ -1,4 +1,4 @@
-package main
+package login
 
 import (
 	"crypto/tls"
@@ -7,7 +7,14 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"strings"
 )
+
+type cookies struct {
+	server     string
+	_csrf      string
+	jsessionID string
+}
 
 func getSessionServer() map[string]string {
 	tr := &http.Transport{
@@ -87,11 +94,53 @@ func getSessionServer() map[string]string {
 func getLoginPage() {
 
 	secrets := getSessionServer()
+	vtopCookies := cookies{
+		server:     secrets["SERVERID"],
+		_csrf:      secrets["_csrf"],
+		jsessionID: secrets["JSESSIONID"],
+	}
 
-	fmt.Println("JSESSIONID:", secrets["JSESSIONID"])
-	fmt.Println("SERVERID:", secrets["SERVERID"])
-	fmt.Println("_csrf:", secrets["_csrf"])
+	fmt.Println("VTOP Cookies:", vtopCookies)
 
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	var data = strings.NewReader(fmt.Sprintf(`_csrf=%s&flag=VTOP`, vtopCookies._csrf))
+	req, err := http.NewRequest("POST", "https://vtop.vit.ac.in/vtop/prelogin/setup", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Host", "vtop.vit.ac.in")
+	req.Header.Set("Content-Length", "52")
+	req.Header.Set("Cache-Control", "max-age=0")
+	req.Header.Set("Sec-Ch-Ua", `"Chromium";v="119", "Not?A_Brand";v="24"`)
+	req.Header.Set("Sec-Ch-Ua-Mobile", "?0")
+	req.Header.Set("Sec-Ch-Ua-Platform", `"Linux"`)
+	req.Header.Set("Upgrade-Insecure-Requests", "1")
+	req.Header.Set("Origin", "https://vtop.vit.ac.in")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.159 Safari/537.36")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+	req.Header.Set("Sec-Fetch-Site", "same-origin")
+	req.Header.Set("Sec-Fetch-Mode", "navigate")
+	req.Header.Set("Sec-Fetch-User", "?1")
+	req.Header.Set("Sec-Fetch-Dest", "document")
+	req.Header.Set("Referer", "https://vtop.vit.ac.in/vtop/open/page")
+	// req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
+	req.Header.Set("Priority", "u=0, i")
+	req.Header.Set("Cookie", fmt.Sprintf("JSESSIONID=%s; SERVERID=%s", vtopCookies.jsessionID, vtopCookies.server))
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", bodyText)
 }
 
 func main() {
