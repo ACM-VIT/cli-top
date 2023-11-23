b239a1afe080255d92cea0730349312005309e23
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-11-24 00:41:26 +0530

    Refact: split the request function
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- features/marks.go
+++ features/marks.go
@@ -5,9 +5,7 @@ package features
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -20,50 +18,13 @@ type SemesterDetails struct {
 	SemIds   []string
 }
 
-func fetchReq(target_url string, semID string) ([]byte, error) {
-	client := &http.Client{}
-
-	//variables that need to be set with every request
+func GetSemDetails() SemesterDetails {
+	url := "https://vtop.vit.ac.in/vtop/academics/common/StudentTimeTable"
 	authID := "21BIT0151"
 	csrf := "f16fa23f-c0b3-4f9f-bc1d-4ba000d96e3e"
 	jsessionID := "1BB3D5A9A32750B4945B8643EAAA514F"
 
-	var data = strings.NewReader(fmt.Sprintf("------WebKitFormBoundary9yjNZXu7BBjgQK7J\r\nContent-Disposition: form-data; name=\"authorizedID\"\r\n\r\n%s\r\n------WebKitFormBoundary9yjNZXu7BBjgQK7J\r\nContent-Disposition: form-data; name=\"semesterSubId\"\r\n\r\n%s\r\n------WebKitFormBoundary9yjNZXu7BBjgQK7J\r\nContent-Disposition: form-data; name=\"_csrf\"\r\n\r\n%s\r\n------WebKitFormBoundary9yjNZXu7BBjgQK7J--\r\n", authID, semID, csrf))
-	req, err := http.NewRequest("POST", target_url, data)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("authority", "vtop.vit.ac.in")
-	req.Header.Set("accept", "*/*")
-	req.Header.Set("accept-language", "en-US,en;q=0.6")
-	req.Header.Set("content-type", "multipart/form-data; boundary=----WebKitFormBoundary9yjNZXu7BBjgQK7J")
-	req.Header.Set("cookie", fmt.Sprintf("JSESSIONID=%s; SERVERID=s2", jsessionID))
-	req.Header.Set("origin", "https://vtop.vit.ac.in")
-	req.Header.Set("referer", "https://vtop.vit.ac.in/vtop/content?")
-	req.Header.Set("sec-ch-ua", `"Brave";v="119", "Chromium";v="119", "Not?A_Brand";v="24"`)
-	req.Header.Set("sec-ch-ua-mobile", "?1")
-	req.Header.Set("sec-ch-ua-platform", `"Android"`)
-	req.Header.Set("sec-fetch-dest", "empty")
-	req.Header.Set("sec-fetch-mode", "cors")
-	req.Header.Set("sec-fetch-site", "same-origin")
-	req.Header.Set("sec-gpc", "1")
-	req.Header.Set("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36")
-	req.Header.Set("x-requested-with", "XMLHttpRequest")
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	bodyText, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	return bodyText, nil
-}
-
-func GetSemDetails() SemesterDetails {
-	url := "https://vtop.vit.ac.in/vtop/academics/common/StudentTimeTable"
-	bodyText, err := fetchReq(url, "")
+	bodyText, err := fetchReq(cookies{authID, csrf, jsessionID}, url, "")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -199,7 +160,10 @@ func getTextContent(n *html.Node) string {
 
 func GetMarks(semID string) {
 	url := "https://vtop.vit.ac.in/vtop/examinations/doStudentMarkView"
-	bodyText, err := fetchReq(url, semID)
+	authID := "21BIT0151"
+	csrf := "f16fa23f-c0b3-4f9f-bc1d-4ba000d96e3e"
+	jsessionID := "1BB3D5A9A32750B4945B8643EAAA514F"
+	bodyText, err := fetchReq(cookies{authID, csrf, jsessionID}, url, semID)
 	if err != nil {
 		log.Fatal(err)
 	}
