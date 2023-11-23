62d6d6b71afceabea368a31fdc81843b561694ea
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-11-24 03:11:41 +0530

    Update: call login from main
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- main.go
+++ main.go
@@ -2,10 +2,14 @@
 
 package main
 
-import "vtop-cli/features"
+import (
+	// "vtop-cli/features"
+	login "vtop-cli/login"
+)
 
 func main() {
 	// Call Marks function with an integer
+	login.Login()
+	// features.Marks(0)
 
-	features.Marks(0)
 }
