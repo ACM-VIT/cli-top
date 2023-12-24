2a6c183e21b190f27803c0323592bc085ac20ec1
shambhavipaygude <you@example.com>
2023-12-24 20:38:20 +0530

    Added grade view

--- main.go
+++ main.go
@@ -5,28 +5,30 @@ import (
 	"fmt"
 	// "vtop-cli/features"
 	//"gopkg.in/yaml.v3"
-	"gomodules/attendancecalculator"
+	// "VTOP-CLI/attendancecalculator"
 	//test "gomodules/test"
-	types "gomodules/types"
+	types "VTOP-CLI/types"
+	"VTOP-CLI/gradeView"
 )
 
 func main() {
 	cookies := types.Cookies{
-		SERVERID:   "s2",
-		CSRF:       "b5cc5f45-9372-43ac-8e07-559df2f4f941",
-		JSESSIONID: "C932917D52B590F2B0C0208EA482E080",
+		SERVERID:   "s1",
+		CSRF:       "7f6e6f77-c3d2-482b-bdb2-f74bdcf97356",
+		JSESSIONID: "C9F77ED32FE5E018A74B6428FD67AA05",
 	}
 
 	// Creating a LogIn instance
 	login := types.LogIn{
 		Username: "22BCI0272",
-		Password: "Hemlo@1005",
+		Password: "",
 	}
 
-	attendancecalculator.PrintSemDetails(login.Username, cookies)
-
-	//semId := attendancecalculator.Marks(login.Username, cookies, 0)
-	attendancecalculator.GetAttendance(login.Username, cookies, "")
+	// attendancecalculator.PrintSemDetails(login.Username, cookies)
+	// attendancecalculator.GetAttendance(login.Username, cookies, "")
+	gradeView.PrintSemDetails(login.Username,cookies)
+	gradeView.GetGrade(login.Username,cookies,"")
 	fmt.Println("")
 
 }
+
