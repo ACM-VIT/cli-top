d8d87ff4dd2f536a1d16499d59d02e57f2d0db4a
gptsarthak <sarthakgpt95@gmail.com>
2024-01-21 22:09:01 +0530

    feat: check for invalid password and maximum failed attempts

--- login/login.go
+++ login/login.go
@@ -54,20 +54,65 @@ func performLogin(userInfo types.LogIn, cookies types.Cookies, captcha string) t
 	}
 	defer resp.Body.Close()
 
-	bodyText, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// fmt.Printf("From login page, after submitting captcha:\n%s\n", bodyText)
-
-	if strings.Contains(string(bodyText), "Invalid Captcha") {
-		fmt.Println("Invalid Captcha. The captcha solver can sometimes confuse between B and 8, please retry...")
+	if errorCheck(cookies) {
 		return types.Cookies{}
 	}
 
 	tokens := helpers.ExtractCookies(resp)
 
 	return tokens
+}
+
+func errorCheck(cookies types.Cookies) bool {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	req, err := http.NewRequest("GET", "https://vtop.vit.ac.in/vtop/login/error", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Host", "vtop.vit.ac.in")
+	req.Header.Set("Cache-Control", "max-age=0")
+	req.Header.Set("Upgrade-Insecure-Requests", "1")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.159 Safari/537.36")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+	req.Header.Set("Sec-Fetch-Site", "same-origin")
+	req.Header.Set("Sec-Fetch-Mode", "navigate")
+	req.Header.Set("Sec-Fetch-User", "?1")
+	req.Header.Set("Sec-Fetch-Dest", "document")
+	req.Header.Set("Sec-Ch-Ua", `"Chromium";v="119", "Not?A_Brand";v="24"`)
+	req.Header.Set("Sec-Ch-Ua-Mobile", "?0")
+	req.Header.Set("Sec-Ch-Ua-Platform", `"Linux"`)
+	req.Header.Set("Referer", "https://vtop.vit.ac.in/vtop/login")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
+	req.Header.Set("Priority", "u=0, i")
+	req.Header.Set("Cookie", fmt.Sprintf("JSESSIONID=%s; SERVERID=%s", cookies.JSESSIONID, cookies.SERVERID))
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if strings.Contains(string(bodyText), "Invalid Captcha") {
+		fmt.Println("\nInvalid Captcha. The captcha solver can sometimes confuse between B and 8, please retry...\n")
+		return true
+	}
+	if strings.Contains(string(bodyText), "Invalid LoginId/Password") {
+		fmt.Println("\nInvalid Password. Please check your password and try again...\n")
+		return true
+	}
+
+	if strings.Contains(string(bodyText), "Maximum Fail Attempts") {
+		fmt.Println("\nNumber Of Maximum Fail Attempts Reached.Use Forgot Password on VTOP to reset your password.\n")
+		return true
+	}
+
+	return false
 }
 
 func Login(regNo string, password string) types.Cookies {
