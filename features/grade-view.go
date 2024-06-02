52fe258785c7874ccf053ff61a7941354862a524
saharshbhansali <saharsh.bhansali15@gmail.com>
2024-06-02 15:49:27 +0530

    Refact: attendance calculator and grade view refactored and tested on Linux.
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- features/grade-view.go
+++ features/grade-view.go
@@ -8,13 +8,12 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
-	"github.com/charmbracelet/glamour"
 )
 
 func GetGrades(regNo string, cookies types.Cookies, semId string, sem_choice int) {
 	url := "https://vtop.vit.ac.in/vtop/examinations/examGradeView/doStudentGradeView"
 
-	semesterID := Attendance(regNo, cookies, sem_choice)
+	semesterID := helpers.SelectSemester(regNo, cookies, sem_choice)
 	bodyText, err := helpers.FetchReq(regNo, cookies, url, semesterID, "UTC", "POST")
 	if err != nil {
 		log.Fatal(err)
@@ -27,53 +26,6 @@ func GetGrades(regNo string, cookies types.Cookies, semId string, sem_choice int
 	}
 
 	findAndSaveGrade(doc)
-
-}
-
-func Grade(regNo string, cookies types.Cookies, sem_choice int) string {
-	selectedSemId := ""
-	selectedSemName := ""
-
-	var choice int
-	if sem_choice == 0 {
-		PrintSemDetails(regNo, cookies)
-		fmt.Print("\nEnter the index of the semester to view grade: ")
-		fmt.Scanln(&choice)
-	} else {
-		choice = sem_choice
-	}
-	semDet := helpers.GetSemDetails(cookies, regNo)
-
-	if choice < 1 || choice > len(semDet.SemIds) {
-		fmt.Println("Invalid choice.")
-	} else {
-		for i, id := range semDet.SemIds {
-			if i+1 == choice {
-				selectedSemId = id
-				selectedSemName = semDet.SemNames[i]
-			}
-		}
-	}
-
-	// Format the string with glamour
-	formattedSelection := fmt.Sprintf("\n# You selected SemId: %s, SemName: %s\n", selectedSemId, selectedSemName)
-
-	// Render and print the formatted string
-	renderer, err := glamour.NewTermRenderer(glamour.WithStylePath("dark"), glamour.WithWordWrap(150))
-	if err != nil {
-		log.Fatal("Error creating glamour renderer:", err)
-	}
-
-	output, err := renderer.Render(formattedSelection)
-	if err != nil {
-		log.Fatal("Error rendering formatted string:", err)
-	}
-
-	fmt.Print(output)
-
-	fmt.Println()
-
-	return selectedSemId
 
 }
 
