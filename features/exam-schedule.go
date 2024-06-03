df1e5f6d8c6d2e21f28e82c1ad5dc7ca793d5f30
saharshbhansali <saharsh.bhansali15@gmail.com>
2024-06-03 23:24:51 +0530

    Refact: standardized marks feature. updated helpers/curl-requests.go (FetchReq). Broken: receipt.go (receipt) feature still uses deprecated features.fetchReq. All other dependant files and functions havve been updated. Tested locally on Linux.
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- features/exam-schedule.go
+++ features/exam-schedule.go
@@ -19,7 +19,7 @@ func GetExamSchedule(regNo string, cookies types.Cookies, semId string, sem_choi
 
 	semesterID := helpers.SelectSemester(regNo, cookies, sem_choice)
 
-	bodyText, err := helpers.FetchReq(regNo, cookies, url, semesterID, "UTC", "POST")
+	bodyText, err := helpers.FetchReq(regNo, cookies, url, semesterID, "UTC", "POST", "")
 	if err != nil {
 		log.Fatal(err)
 	}
