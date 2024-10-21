80e262e0160086f558999bfb93f8201ad7f1f1d9
shruthilaya <79406715+shruthilayak11@users.noreply.github.com>
2024-10-21 23:43:40 +0530

    Update class-message.go

--- features/class-message.go
+++ features/class-message.go
@@ -1,20 +1,26 @@
 package features
 
 import (
+	//"bytes"
+	//"io"
 	"cli-top/debug"
 	"cli-top/helpers"
 	"cli-top/types"
 	"fmt"
+	"regexp"
+
+	//"net/http"
 	"strings"
+	//"time"
+
 	"github.com/PuerkitoBio/goquery"
 )
 
 
 func GetClassMessage(regNo string, cookies types.Cookies) {
-	
-	url := "https://vtop.vit.ac.in/vtop/academics/common/StudentClassMessage"
 
-	bodyText, err := helpers.FetchReq(regNo, cookies, url, "", "UTC", "GET", "")
+	url := "https://vtop.vit.ac.in/vtop/academics/common/StudentClassMessage"
+	bodyText, err := helpers.FetchReq(regNo, cookies, url, "", "UTC", "POST", "")
 	if err != nil && debug.Debug {
 		fmt.Println(err)
 		return
@@ -25,53 +31,40 @@ func GetClassMessage(regNo string, cookies types.Cookies) {
 		fmt.Println("Error extracting messages:", err)
 		return
 	}
-
-	printClassMessagesTable(messages)
+	fmt.Println()	
+	helpers.PrintTable(messages)
+	fmt.Println()
 }
 
 
 func extractClassMessages(bodyText []byte) ([][]string, error) {
 	var messages [][]string
-
-	
+	messages = append(messages, []string{"Course", "Message"})
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(bodyText)))
 	if err != nil {
 		return nil, fmt.Errorf("error parsing HTML: %v", err)
 	}
 
-	
-	doc.Find(".panel.panel-default").Each(func(i int, panel *goquery.Selection) {
-		var row []string
+	re := regexp.MustCompile(^[A-Z0-9]+ - | - Online Course)
 
-		
-		course := panel.Find("span:contains('Course:')").Next().Text()
-		course = strings.TrimSpace(course)
-
-		
-		message := panel.Find("b:contains('Message:')").Next().Text()
-		message = strings.TrimSpace(message)
-
-		
-		if course != "" && message != "" {
-			row = append(row, course, message)
+    doc.Find("h5").Each(func(i int, h5 *goquery.Selection) {
+        var row []string
+        h5.Find("span").Each(func(i int, span *goquery.Selection) {
+			trimmedText := strings.TrimSpace(span.Text())
+            cleanedText := re.ReplaceAllString(trimmedText, "")
+            cleanedText = strings.ReplaceAll(cleanedText, "\n", " ")
+            if len(cleanedText) > 60 {
+                cleanedText = cleanedText[:60] + "..."
+            }
+            row = append(row, cleanedText)
+        })
+		if len(row) == 2 {
 			messages = append(messages, row)
 		}
-	})
+    })
 
-	
 	if len(messages) == 0 {
 		return nil, fmt.Errorf("no class messages found")
 	}
-
 	return messages, nil
-}
-
-
-func printClassMessagesTable(messages [][]string) {
-	fmt.Printf("| %-35s | %-100s |\n", "Course", "Message")
-	fmt.Printf("|%-37s|%-102s|\n", strings.Repeat("-", 37), strings.Repeat("-", 102))
-
-	for _, row := range messages {
-		fmt.Printf("| %-35s | %-100s |\n", row[0], row[1])
-	}
 }
