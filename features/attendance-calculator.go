7a9d9aa2c6fcb24f1c9a2b9004cfbfb73287890a
Adheesh Garg <adheeshgarg0611@gmail.com>
2024-10-24 01:45:05 +0530

    fix: attendence 75% alert

--- features/attendance-calculator.go
+++ features/attendance-calculator.go
@@ -5,6 +5,8 @@ import (
 	"cli-top/helpers"
 	types "cli-top/types"
 	"fmt"
+	"math"
+
 	//"math"
 	"regexp"
 	"strconv"
@@ -15,14 +17,14 @@ import (
 
 func GetAttendance(regNo string, cookies types.Cookies, semId string, sem_choice int) {
 	url := "https://vtop.vit.ac.in/vtop/processViewStudentAttendance"
-	semDetails,err := helpers.GetSemDetails(cookies, regNo)
+	semDetails, err := helpers.GetSemDetails(cookies, regNo)
 	if err != nil && debug.Debug {
-        fmt.Printf("Error fetching semesters: %v\n", err)
+		fmt.Printf("Error fetching semesters: %v\n", err)
 
-    }
+	}
 	if len(semDetails) == 0 {
 		fmt.Println("No semesters found.")
-		return 
+		return
 	}
 	semID := semDetails[len(semDetails)-1].SemID
 	bodyText, err := helpers.FetchReq(regNo, cookies, url, semID, "UTC", "POST", "")
@@ -37,11 +39,11 @@ func GetAttendance(regNo string, cookies types.Cookies, semId string, sem_choice
 	}
 	attendenceList := findAndSaveAttendance(doc)
 	fmt.Println()
-	helpers.PrintTable(attendenceList,1)
+	helpers.PrintTable(attendenceList, 1)
 	fmt.Println()
 }
 
-func findAndSaveAttendance(doc *goquery.Document) [][]string{
+func findAndSaveAttendance(doc *goquery.Document) [][]string {
 	var attendanceList [][]string
 	attendanceList = append(attendanceList, []string{"Subject", "Type", "Faculty Name", "Classes Attended", "Percentage", "75% Alert"})
 	// var markdownTable strings.Builder
@@ -56,27 +58,30 @@ func findAndSaveAttendance(doc *goquery.Document) [][]string{
 			total := rowSelection.Find("td").Eq(6).Find("span").Text()
 			percent := rowSelection.Find("td").Eq(7).Find("span").Find("span").Text()
 			reSub := regexp.MustCompile(`-\s*(.*?)\s*-`)
-            match := reSub.FindStringSubmatch(sub_name_and_type)
-            if len(match) > 1 {
-                sub_name = strings.TrimSpace(match[1])
-            }
-            reSubType := regexp.MustCompile(`[^-]*$`)
+			match := reSub.FindStringSubmatch(sub_name_and_type)
+			if len(match) > 1 {
+				sub_name = strings.TrimSpace(match[1])
+			}
+			reSubType := regexp.MustCompile(`[^-]*$`)
 			matchType := reSubType.FindString(sub_name_and_type)
 			sub_type = strings.TrimSpace(matchType)
 
-            reProf := regexp.MustCompile(`^(.*?)\s*-\s*`)
-            matchProf := reProf.FindStringSubmatch(proff)
-            if len(matchProf) > 1 {
-                proff = strings.Title(strings.ToLower(matchProf[1]))
-            }
+			reProf := regexp.MustCompile(`^(.*?)\s*-\s*`)
+			matchProf := reProf.FindStringSubmatch(proff)
+			if len(matchProf) > 1 {
+				proff = strings.Title(strings.ToLower(matchProf[1]))
+			}
 			classes_attended := attended + "/" + total
 			attendedInt, _ := strconv.Atoi(attended)
 			totalInt, _ := strconv.Atoi(total)
-			if (sub_type == "Lab Only") {
+			var missOrAttend string
+			if sub_type == "Lab Only" || sub_type == "Embedded Lab" {
 				attendedInt = attendedInt/2
 				totalInt = totalInt/2
+				missOrAttend = calculateAttendance(attendedInt, totalInt)
+			} else {
+				missOrAttend = calculateAttendance(attendedInt, totalInt)
 			}
-            missOrAttend := calculateAttendance(attendedInt, totalInt)
 			attendanceList = append(attendanceList, []string{sub_name, sub_type, proff, classes_attended, percent, missOrAttend})
 		})
 	} else {
@@ -87,14 +92,19 @@ func findAndSaveAttendance(doc *goquery.Document) [][]string{
 }
 
 func calculateAttendance(attended, total int) string {
-    requiredAttendance := int(0.75 * float64(total))
-    if attended >= requiredAttendance {
-		// Calculate how many classes can be missed
-		canMiss := attended - requiredAttendance
-		return fmt.Sprintf("\033[32mCan miss %d classes\033[0m", canMiss) // Red color
+	// Calculate how many more classes need to be attended to meet 74.01% attendance
+	targetAttendance := 0.7401
+	neededAttendance := targetAttendance * float64(total)
+
+	// If the current attendance is already below the target
+	if float64(attended) < neededAttendance {
+		// Calculate the exact number of additional classes required to meet 74.01%
+		x := (targetAttendance*float64(total) - float64(attended)) / (1 - targetAttendance)
+		x = math.Ceil(x) // Round up to ensure they meet the target after attending whole classes
+		return fmt.Sprintf("\033[31mAttend %d more classes\033[0m", int(x))
 	} else {
-		// Calculate how many more classes need to be attended
-		needToAttend := requiredAttendance - attended
-		return fmt.Sprintf("\033[31mAttend %d classes\033[0m", needToAttend) // Green color
+		// If already at or above the target, calculate how many can be missed
+		canMiss := attended - int(math.Ceil(neededAttendance))
+		return fmt.Sprintf("\033[32mCan miss %d classes\033[0m", canMiss)
 	}
 }
