811ca2bafd3ba3f0e8fe6b5d61f44cc901a26e0f
theg <52027622+theg1239@users.noreply.github.com>
2024-11-12 17:47:11 +0530

    chore: minor

--- helpers/sem-details.go
+++ helpers/sem-details.go
@@ -85,7 +85,7 @@ func SelectSemester(regNo string, cookies types.Cookies, sem_choice int) (types.
     // clearInputBuffer() // Uncomment if necessary and ensure it doesn't cause blocking
 
     var nested_sem_list [][]string
-    nested_sem_list = append(nested_sem_list, []string{"SemId", "SemName"})
+    nested_sem_list = append(nested_sem_list, []string{"Semester ID", "Semester"})
     for i := 0; i < len(semDetails); i++ {
         nested_sem_list = append(nested_sem_list, []string{semDetails[i].SemID, semDetails[i].SemName})
     }
