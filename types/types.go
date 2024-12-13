8c5150ed73bb7484447b7defdadc144014a4e342
Adheesh Garg <adheeshgarg0611@gmail.com>
2024-12-14 02:41:06 +0530

    fix: refactor time table code and timetable ics

--- types/types.go
+++ types/types.go
@@ -146,3 +146,20 @@ type CourseDetail struct {
 	Faculty     string
 	Slot        string
 }
+
+type Class struct {
+	Subject string
+	Slot    string
+	Venue   string
+	StartTime string
+	EndTime string
+}
+
+
+
+//  Slot map[strint]map[string][]string
+
+type SubjectTime struct {
+	Slot   []string
+	Venue  string
+}
