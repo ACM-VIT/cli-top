c21953ed1a18677737bfb8b991331f4287996a99
Adheesh Garg <adheeshgarg0611@gmail.com>
2025-06-06 20:07:04 +0530

    feat: setup for config file

--- helpers/helper.go
+++ helpers/helper.go
@@ -588,3 +588,16 @@ func ParseFloat(s string) float64 {
 }
 
 var VtopLoginGlobal func() (types.Cookies, string)
+
+
+func CompareStringSlices(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
